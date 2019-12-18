4813ea86dc5f11bf9b75922c28fb6217d402378a
Hammed <hammed@hollatags.com>
2019-12-18 14:28:19 +0100

    Updates

---
+++ precache-manifest.fb1d9ad533f238b7fd67b31b0679d6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa060e2486e32ddb1693f0883fba8d43",
+    "url": "/teamwork/index.html"
+  },
+  {
+    "revision": "53db70cfb60c2a77636f",
+    "url": "/teamwork/static/css/main.ce62760e.chunk.css"
+  },
+  {
+    "revision": "d91eecb86f9ac3161127",
+    "url": "/teamwork/static/js/2.116ace67.chunk.js"
+  },
+  {
+    "revision": "53db70cfb60c2a77636f",
+    "url": "/teamwork/static/js/main.eba0cb86.chunk.js"
+  },
+  {
+    "revision": "623fc6ecca87782c15ef",
+    "url": "/teamwork/static/js/runtime-main.0f497ae7.js"
+  },
+  {
+    "revision": "91199c07f4f51678e6b2174a3e598c5e",
+    "url": "/teamwork/static/media/team-work.91199c07.jpg"
+  }
+]);
